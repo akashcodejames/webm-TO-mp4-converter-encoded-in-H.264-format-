1e7e538985bb6f7be2ae8711d0a6b3b5aefc90ee
AkAsH Yadav <111753799+akashcodejames@users.noreply.github.com>
2023-03-11 03:23:30 +0530

    Update webmconvertTomp4.py
    Main python code to convert the video webm video to mp4 format which is seekable means you can move forward and backward.

--- webmconvertTomp4.py
+++ webmconvertTomp4.py
@@ -1,40 +1,35 @@
 import os
 import sys
 
-path = "A:/Module 10"
-print("tim")
-dir_list1 = os.listdir(path)
-hmm = 0
-while True:
+def my_func(path):
     try:
-        while hmm < len(dir_list1):
-            dir_list = os.listdir(path + "/" + dir_list1[hmm])
-            i = 0
-            while i < len(dir_list):
-                if "webm" in dir_list[i]:
-                    list_1 = dir_list[i].split("webm")
-                    a = list_1[0] + "mp4"
-                    print(a)
-                    try:
-                        os.system(f'cmd /c "ffmpeg -i "{path + "/" + dir_list1[hmm] + "/" + dir_list[i]}" -c:v copy "{ path + "/" + dir_list1[hmm] + "/" + a}" "')
-                    except:
-                        sys.exit(0)
-                    os.remove(path + "/" + dir_list1[hmm] + "/" + dir_list[i])
-                    i = i + 1
-                else:
-                    print("ok")
-                    i = i + 1
-            hmm = hmm + 1
-            if hmm == len(dir_list1):
-                hmm = "abc" + 1
+        dir_list = os.listdir(path)
     except:
-        if hmm == len(dir_list1):
-            break
+        return
+    i = 0
+    while i < len(dir_list):
+        if "webm" in dir_list[i]:
+            list_1 = dir_list[i].split("webm")
+            a = list_1[0] + "mp4"
+            print(a)
+            try:
+                os.system(
+                    f'cmd /c "ffmpeg -i "{path + "/" + dir_list[i]}" -c:v copy "{path + "/" + a}" "')
+            except:
+                sys.exit(0)
+            os.remove(path + "/" + dir_list[i])
+            i = i + 1
         else:
-            hmm = hmm + 1
+            my_func(path + "/" + dir_list[i]) # this recursive call will enable to visit all directories where a file can exist and will search for webm video
+            i = i + 1
 
-# prints all files
-print(len(dir_list))
 
-# os.system('cmd /k "ffmpeg -i "A:/Module 8/1 - Deep LearningNeural Networks/1.1 - History of Neural networks and Deep '
-#           'Learning..webm" getsomeen.mp4 "')
+
+paths = "A:\Module 1"         # path which contains all the webm videos , it could contain other files , 
+                              # this program can search for all directories within provided directory and search for webm videos and convert it mp4 )
+                              # It will not effect your other files
+my_func(paths)
+ 
+    
+    
+   
