f4114982a411f286be141d0d2e2c2d7805f357d6
AkAsH Yadav <111753799+akashcodejames@users.noreply.github.com>
2023-03-11 05:06:39 +0530

    Update webmconvertTomp4.py

--- webmconvertTomp4.py
+++ webmconvertTomp4.py
@@ -8,16 +8,16 @@ def my_func(path):
         return
     i = 0
     while i < len(dir_list):
-        if "webm" in dir_list[i]:
+        if "webm" in dir_list[i]:           # selecting file if it is webm video
             list_1 = dir_list[i].split("webm")
             a = list_1[0] + "mp4"
             print(a)
             try:
                 os.system(
-                    f'cmd /c "ffmpeg -i "{path + "/" + dir_list[i]}" -c:v copy "{path + "/" + a}" "')
+                    f'cmd /c "ffmpeg -i "{path + "/" + dir_list[i]}" -c:v copy "{path + "/" + a}" "')  # Executing CMD command 
             except:
                 sys.exit(0)
-            os.remove(path + "/" + dir_list[i])
+            os.remove(path + "/" + dir_list[i])  # to remove previous webm video since mp4 is already saved, this line does not gets executed if some error occur at 16th line hence ensuring your data is not lost in any case
             i = i + 1
         else:
             my_func(path + "/" + dir_list[i]) # this recursive call will enable to visit all directories where a file can exist and will search for webm video
