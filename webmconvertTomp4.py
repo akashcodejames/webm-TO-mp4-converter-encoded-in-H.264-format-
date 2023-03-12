62badd214c0064c597d93aa0d3000e8be9a330fd
AkAsH Yadav <111753799+akashcodejames@users.noreply.github.com>
2023-03-13 00:38:21 +0530

    Update webmconvertTomp4.py

--- webmconvertTomp4.py
+++ webmconvertTomp4.py
@@ -3,12 +3,12 @@ import sys
 
 def my_func(path):
     try:
-        dir_list = os.listdir(path)
+        dir_list = os.listdir(path) # trying to get all file names in (path) directory in list dir_list
     except:
         return
     i = 0
     while i < len(dir_list):
-        if "webm" in dir_list[i]:           # selecting file if it is webm video
+        if "webm" in dir_list[i]:           # checking the i th element in dir_list is webm type 
             list_1 = dir_list[i].split("webm")
             a = list_1[0] + "mp4"
             print(a)
@@ -20,7 +20,10 @@ def my_func(path):
             os.remove(path + "/" + dir_list[i])  # to remove previous webm video since mp4 is already saved, this line does not gets executed if some error occur at 16th line hence ensuring your data is not lost in any case
             i = i + 1
         else:
-            my_func(path + "/" + dir_list[i]) # this recursive call will enable to visit all directories where a file can exist and will search for webm video
+            my_func(path + "/" + dir_list[i])  # If the file is not a webm type, there is a chance that it is a directory with further files 
+                                               # that could also contain webm video, hence we must also investigate this file further.
+                
+                                               # this recursive call will enable to visit all directories where a file can exist and will search for webm video
             i = i + 1
 
 
